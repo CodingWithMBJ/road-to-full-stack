567d45235b03a414df624b1ef4f96ace9a308904
Mohamed Jalloh <codingwithmbj@gmail.com>
2025-09-07 23:20:20 -0500

    day 2

---
+++ day-2/Data types/src/data_types.java
@@ -0,0 +1,11 @@
+public class data_types {
+    public static void main(String[] args) {
+        int favNum = 7;
+        float price = 3.99F;
+        char gender = 'M';
+        boolean married = false;
+        String middleName = "Bashir";
+
+        System.out.println("My middle name is " + middleName + " and my gender is a " + gender);
+    }
+}
---
+++ day-2/variables/Variables/src/variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args) {
+        String firstName = "Mohamed";
+        String lastName = "Jalloh";
+
+        int age = 33;
+
+        double currentTime = 7.55;
+
+        boolean amITired = true;
+
+        char firstNameInitials = 'M';
+
+
+        System.out.println("my name is " + firstName + " " + lastName + " and I am " + age + " years old");
+    }
+}
